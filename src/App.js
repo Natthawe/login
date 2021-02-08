068991cb95a800a662a9b817085f38c69e8cd886
Natthawe Jumjai <natthawejumjai@gmail.com>
2021-02-08 17:07:47 +0700

    add form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{ useState } from 'react';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  const adminUser = {
+    email:"admin@admin.com",
+    password:"a"
+  }
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if(details.email === adminUser.email && details.password === adminUser.password){
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email:details.email
+      });
+    } else{
+      console.log("Details don't Match!");
+      setError("Details don't Match!");
+    }
+  }
+  const Logout = details => {
+    setUser({name: "", email: ""});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {(user.email !== "") ? (
+          <form>
+          <div className="welcome">
+            <h2>Welcome  <span>{user.name}</span></h2>
+            <button className="btn" onClick={Logout}>Logout</button>
+          </div>
+          </form>
+        ) : (
+              <LoginForm Login = {Login} error={error}/>
+        )}
     </div>
   );
 }
